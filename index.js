187db7bf3ae490eed6b53a8916c57c28c0ac23af
Syed Mahmoud Sobhani <84802517+Danger-XD@users.noreply.github.com>
2024-10-16 19:42:27 +0600

    frontend added

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from "express";
 const app = express();
 const PORT = 2323;
-import bodyParser from "body-parser";
 import router from "./routes/api.js";
 
 //Add body-parser middleware to parse incoming request bodies
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use("/v1",router);
